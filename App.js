9bcac50419ed3aa6139e1310705424d60a326f5c
rajanv21 <159936455+rajanv21@users.noreply.github.com>
2024-06-20 22:02:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>IOS CALCULATOR</h1>
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
